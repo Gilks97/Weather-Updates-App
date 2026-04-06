88a9e2985d59ea385aee33c5d1c6545dd32ebf41
Gilks <mosetig97@gmail.com>
2026-04-06 17:44:20 +0300

    Add CI/CD pipeline and homepage tests

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class WeatherAppTests(TestCase):
+    def test_homepage_status_code(self):
+        """Verify the weather app homepage loads correctly."""
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
